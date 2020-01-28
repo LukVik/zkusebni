966b1ca89d234777d4a0c3981b9db9526c22ffa9
LukVik <lukaswvik@gmail.com>
2020-01-28 23:04:41 +0100

    repair test for should get hhome

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get :home
+    get home_url
     assert_response :success
   end
 
