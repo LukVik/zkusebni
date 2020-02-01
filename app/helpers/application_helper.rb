97e5cc43940e29d7cc0679a33486eec11d8f5e6a
LukVik <lukaswvik@gmail.com>
2020-02-01 10:43:45 +0100

    refactoring full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   # Method def, optional arg
   def full_title(page_title = '')
     # Variable assignment
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = 'Ruby on Rails Tutorial Sample App'
     # Boolean test
     if page_title.empty?
       base_title
     else
-      page_title + " | " + base_title
+      "#{page_title} | #{base_title}"
     end
   end
 end
