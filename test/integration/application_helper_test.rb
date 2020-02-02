96b059b4ed2808956f59c3668f0ed06c166e7896
LukVik <lukaswvik@gmail.com>
2020-02-02 21:34:14 +0100

    application_helper_test with loop of page_title layout_sitelinks test update

---
+++ test/integration/application_helper_test.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class ApplicationHelperTest < ActionDispatch::IntegrationTest
+  test 'full title helper test' do
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    assert_equal full_title, base_title
+    page_title = %w[Home About Help Contact]
+    page_title.each do |page_title|
+    assert_equal full_title(page_title), "#{page_title} | #{base_title}"
+    end
+  end
+
+  # test 'full title helper test' do
+  #   base_title = 'Ruby on Rails Tutorial Sample App'
+  #   assert_equal full_title, base_title
+  #   page_title = %w[Home About Help Contact]
+  #   assert_equal full_title(page_title), "#{page_title} | #{base_title}"
+  # end
+
+end
---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test 'sitelinks test' do
+    get root_path
+    assert response.body.include?(full_title('Home'))
+    assert_select 'h1', text: 'Welcome to the Sample App'
+    assert_select '.container' do
+      assert_select 'a[href=?]#logo', root_path
+      assert_select 'nav' do
+        assert_select 'ul.nav.navbar-nav.navbar-right' do
+          assert_select 'li a[href=?]', root_path
+          assert_select 'li a[href=?]', help_path
+        end
+      end
+    end
+    assert_select 'footer' do
+      assert_select 'li a[href=?]', about_path
+      assert_select 'li a[href=?]', contact_path
+    end
+    get contact_path
+    assert_select 'title', full_title('Contact')
+    assert response.body.include?(full_title('Contact'))
+  end
+end
