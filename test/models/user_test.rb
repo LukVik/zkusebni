7d3d2d2fb32237c4ccc9bb30e1f42c399c10980a
LukVik <lukaswvik@gmail.com>
2020-02-08 11:14:15 +0100

    user model tests of validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -73,4 +73,13 @@ class UserTest < ActiveSupport::TestCase
   end
 
   # Active Record uniqueness validation does not guarantee uniqueness at the database level
+  test "email addresses should be saved as lower-case" do
+    # insert usecase to variable
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+
 end
