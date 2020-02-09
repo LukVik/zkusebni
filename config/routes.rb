5dad9daf44aff480145af7998a9f642285301ba2
LukVik <lukaswvik@gmail.com>
2020-02-09 18:15:02 +0100

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
   root 'static_pages#home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  resources :users
+  get '/signup', to: 'users#new' # NEW registration
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
