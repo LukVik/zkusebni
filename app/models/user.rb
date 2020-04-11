fc2eface4831afd6c0a3f2a9bb75c2c7d9c742ab
LukVik <lukaswvik@gmail.com>
2020-04-11 18:32:28 +0200

    index page test

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  self.per_page = 10
+  self.per_page = 15
   # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
