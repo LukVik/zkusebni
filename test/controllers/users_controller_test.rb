9ae59d50df4ef44a0eaba5f138d705b3660414ed
LukVik <lukaswvik@gmail.com>
2020-04-12 16:46:38 +0200

    destroy user by only admin

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,54 +1,95 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:lukas)
+    @user = users(:michael)
     @other_user = users(:archer)
   end
 
-  test "should get new signup page" do
+  test 'should get new signup page' do
     get signup_path
     assert_response :success
     assert_select 'title', text: full_title('Sign up')
   end
 
-  test "should redirect index when not logged in" do
+  test 'should redirect index when not logged in' do
     get users_path
     assert_redirected_to login_url
   end
 
-  test "should get edit user page" do
+  test 'should get edit user page' do
     log_in_as(@user)
     get edit_user_path(@user.id)
     assert_response :success
     assert_select 'title', text: full_title('Edit user')
   end
 
-  test "should redirect edit when not logged in" do
+  test 'should redirect edit when not logged in' do
     get edit_user_path(@user)
     assert_not flash.empty?
     assert_redirected_to login_path
   end
 
-  test "should redirect update when not logged in" do
+  test 'should redirect update when not logged in' do
     patch user_path(@user), params: { user: { name: @user.name,
                                               email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_path
   end
 
-  test "should redirect edit when logged in as wrong user" do
+  test 'should redirect edit when logged in as wrong user' do
     log_in_as(@other_user)
     get edit_user_path(@user)
     assert flash.empty?
-    assert_redirected_to root_url
+    assert_redirected_to root_path
   end
 
-  test "should redirect update when logged in as wrong user" do
+  test 'should redirect update when logged in as wrong user' do
     log_in_as(@other_user)
     patch user_path(@user), params: { user: { name: @user.name,
                                               email: @user.email } }
     assert flash.empty?
-    assert_redirected_to root_url
+    assert_redirected_to root_path
+  end
+
+  # by nemělo umožňovat úpravu atributu admin prostřednictvím webu
+  test 'should not allow the admin attribute to be edited via the web' do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+      user: { password: 'password',
+              password_confirmation: 'password',
+              admin: true }
+    }
+    assert_not @other_user.reload.admin?
+  end
+
+  # by mělo umožnit úpravu atributu admin prostřednictvím webu
+  test 'should allow the admin attribute to be edited via the web' do
+    log_in_as(@user)
+    assert @user.admin?
+    patch user_path(@user), params: {
+      user: { password: 'password',
+              password_confirmation: 'password',
+              admin: true }
+    }
+    assert @user.reload.admin?
+  end
+
+  test 'should redirect destroy when not logged in' do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_path
+  end
+
+  test 'should redirect destroy when not logged in as a non-admin' do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_path
   end
 end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
-
   def setup
-    @user = users(:michael)
+    @user = users(:lukas)
+    @admin = users(:michael)
+    @non_admin = users(:archer)
   end
 
   test 'index including pagination' do
@@ -13,5 +16,28 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
     end
+  end
+
+  test 'index as admin including pagination and delete links' do
+    log_in_as(@admin)
+    get users_path
+    assert_select 'div.pagination', count: 2
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'Delete', count: 1
+      end
+    end
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
+  end
+
+  test "index as non-admin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'div.pagination', count: 2
+    assert_select 'a', text: 'Delete', count: 0
   end
 end
